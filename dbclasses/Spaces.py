9e12daf2b511e4b7e23215390c4a4b14fbae7cb7
Kshitij Dhande <kshitijdhande1@gmail.com>
2024-05-28 15:12:28 -0500

    Minor Changes

--- dbclasses/Spaces.py
+++ dbclasses/Spaces.py
@@ -1,19 +1,20 @@
 class Spaces:
-    def __init__(self, zone_id, lot_name, space_number, space_type, availability):
+    def __init__(self, space_number, zone_id, lot_name,  space_type, availability):
+        self.space_number = space_number
         self.zone_id = zone_id
         self.lot_name = lot_name
-        self.space_number = space_number
         self.space_type = space_type
         self.availability = availability
+
+
+    def getSpaceNumber(self):
+        return self.space_number
 
     def getZoneID(self):
         return self.zone_id
 
     def getLotName(self):
         return self.lot_name
-
-    def getSpaceNumber(self):
-        return self.space_number
 
     def getSpaceType(self):
         return self.space_type
