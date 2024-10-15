6592ed0326d7da7a1ad87a7e374a3f45c211fdd3
Kshitij Dhande <kshitijdhande1@gmail.com>
2024-10-14 19:27:53 -0500

    Added all buttons and positioned/styled them accordingly, have to give them functionality

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,16 @@ function App() {
         <h1>Welcome To Parking Management System</h1>
       </div>
 
-      <div className='buttons'>
+      <div className="buttons">
         <a href="/manage-drivers"></a>
         <button className="manage-drivers">Manage Drivers</button>
+        <button className="manage-vehicles">Manage Vehicles</button>
+        <button className="manage-citations">Manage Citations</button>
+        <button className="manage-permit">Manage Permits</button>
+        <button className="manage-parkinglot">Manage Parking Lots</button>
+        <button className="manage-zone">Manage Zones</button>
+        <button className="manage-spaces">Manage Spaces</button>
+        <button className="manage-vehiclecitation">Manage Vehicle Citations</button>
       </div>
     </div>
   );
